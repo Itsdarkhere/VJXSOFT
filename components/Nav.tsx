27f297313602eb5f20b3e775456c5cdc25cc7426
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-02-27 20:10:14 +0200

    Tweaks

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -36,8 +36,8 @@ export default function Nav() {
                             <Link className="pr-0" href="/otayhteytta">Ota yhteyttä</Link>
                         </li>
                     </ul>
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <div className="dropdown dropdown-end block lg:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content text-zinc-950 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
