41fc320383a7c59180aff9f6213da04a3de6f0dd
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-02-27 20:02:24 +0200

    More mobile friendly

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,19 @@ import sss from '../public/sss.svg';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center py-24">
-      <section className="w-full py-10 flex flex-row justify-between max-w-7xl bg-white rounded-lg p-6">
-        <div className="flex flex-col gap-2 justify-center items-start max-w-3xl">
-          <h3 className=" font-bold text-5xl text-zinc-900 leading-tight">Hoidetaanko yrityksesi digitaalinen läsnäolo kuntoon?</h3>
-          <h6 className=" text-zinc-600 text-xl">Löydä asiakkaasi siellä, missä he ovat – hyödynnä dataohjautuvaa markkinointia ja innovatiivisia digistrategioita, jotka vievät yrityksesi seuraavalle tasolle.</h6>
-          <button className=" mt-12 bg-indigo-800 px-12 py-4 text-white font-semibold rounded-md">Ota yhteyttä</button>
+      <section className="px-6 w-full flex justify-center">
+        <div className="w-full py-10 flex flex-col lg:flex-row justify-between max-w-7xl bg-white rounded-lg px-6">
+          <div className="flex flex-col gap-2 justify-center items-start max-w-3xl">
+            <h3 className=" font-bold text-5xl text-zinc-900 leading-tight">Hoidetaanko yrityksesi digitaalinen läsnäolo kuntoon?</h3>
+            <h6 className=" text-zinc-600 text-xl">Löydä asiakkaasi siellä, missä he ovat – hyödynnä dataohjautuvaa markkinointia ja innovatiivisia digistrategioita, jotka vievät yrityksesi seuraavalle tasolle.</h6>
+            <button className=" mt-12 bg-indigo-800 px-12 py-4 text-white font-semibold rounded-md">Ota yhteyttä</button>
+          </div>
+          <Image className=" hidden lg:block" src={sss} alt="VJXSOFT" />
         </div>
-        <Image src={sss} alt="VJXSOFT" />
       </section>
 
-      <section className="w-full mt-10 flex flex-row justify-center">
-        <div className="w-full max-w-7xl py-20 flex flex-row justify-between items-start">
+      <section className="w-full mt-10 flex flex-row justify-center p-6">
+        <div className="w-full max-w-7xl py-20 flex flex-col gap-8 lg:flex-row justify-between items-start">
           <div className="w-full max-w-xl flex flex-col gap-4">
             <h4 className=" text-4xl font-semibold">Ota yhteyttä</h4>
             <p className=" text-zinc-400">Varaa täysin veloitukseton strategiapuhelu oheisella lomakkeella. Täytäthän tiedot mahdollisimman tarkasti, sillä perehdymme yrityksesi tilanteeseen perinpohjaisesti.</p>
@@ -60,6 +62,44 @@ export default function Home() {
             VJXSOFT tarjoaa kaiken osaamisen mitä pienyritys kaipaa digitaalisessa markkinoinnissa. 
             Palvelemme kerralla vain muutamaa asiakasta, jotta voimme keskittyä laatuun ja asiakkaan tarpeisiin.
           </h6>
+        </div>
+      </section>
+
+      <section className="mt-10 w-full flex justify-center p-6">
+        <div className="w-full max-w-7xl py-20 flex flex-col gap-8">
+          <h4 className=" text-4xl font-semibold">Viimeaikaiset blogit</h4>
+          <div className="w-full flex flex-row justify-start gap-6 flex-wrap">
+            <div className="card w-96 bg-base-100 shadow-xl text-zinc-950">
+              <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Paikallinen SEO
+                  <div className="badge badge-secondary">Uusi</div>
+                </h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+
+            <div className="card w-96 bg-base-100 shadow-xl text-zinc-950">
+              <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Pienyrittäjän SEO opas
+                </h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+
+            <div className="card w-96 bg-base-100 shadow-xl text-zinc-950">
+              <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Hakukoneoptimointi eli SEO
+                </h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </main>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -12,7 +12,7 @@ export default function Nav() {
                     <Image height={40} src={logo} alt="VJXSOFT" />
                 </Link>
                 <div>
-                    <ul className="menu menu-horizontal rounded-box">
+                    <ul className="menu menu-horizontal rounded-box pr-0 hidden lg:flex">
                         <li>
                             <details>
                                 <summary className="text-sm text-zinc-300">Blogi</summary>
@@ -32,7 +32,37 @@ export default function Nav() {
                         <li className="text-sm h-10 flex items-center text-zinc-300">
                             <Link href="/hinnasto">Hinnasto</Link>
                         </li>
+                        <li className="text-sm h-10 flex items-center text-zinc-300">
+                            <Link className="pr-0" href="/about">Ota yhteyttä</Link>
+                        </li>
                     </ul>
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content text-zinc-950 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <a>Blogi</a>
+                                <ul className="p-2">
+                                    <li>
+                                        <Link href="/hinnasto">Pienyrittäjän SEO opas</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/hinnasto">Paikallinen SEO</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <Link href="/hinnasto">Palvelut</Link>
+                            </li>
+                            <li>
+                                <Link href="/hinnasto">Hinnasto</Link>
+                            </li>
+                            <li>
+                                <Link href="/hinnasto">Ota yhteyttä</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
