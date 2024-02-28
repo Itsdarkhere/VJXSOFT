ac1849392a8b523e5ea55e576de01c4ce290129f
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-02-28 16:10:05 +0200

    Stuff

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,26 +3,26 @@
 export default function Contact() {
 
     return (
-        <section className="w-full flex bg-white flex-row justify-center lg:px-6">
-            <div className="w-full max-w-7xl bg-gray-900 lg:rounded-3xl p-6 py-24 lg:p-12 flex flex-col gap-8 lg:flex-row justify-between items-start">
+        <section className="w-full border-t border-b flex bg-white flex-row justify-center lg:px-6">
+            <div className="w-full max-w-7xl lg:rounded-3xl p-6 py-24 flex flex-col gap-8 lg:flex-row justify-between items-start">
                 <div className="w-full max-w-xl flex flex-col">
-                    <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-                    <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Ota Yhteyttä</p>
-                    <p className="mt-4 text-lg leading-8 text-gray-300">
+                    <h2 className="text-base font-semibold leading-7 text-indigo-600">Ota Yhteyttä</h2>
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Ota Yhteyttä</p>
+                    <p className="mt-4 text-lg leading-8 text-gray-600">
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
                         iste dolor cupiditate blanditiis ratione.
                     </p>
                 </div>
-                <form className=" p-8 w-full max-w-xl flex flex-col rounded-lg bg-white shadow-md">
+                <form className=" p-8 w-full border max-w-xl flex flex-col rounded-lg bg-base-100 shadow-md">
 
-                    <label className="form-control w-full">
+                    {/* <label className="form-control w-full">
                     <div className="label">
                         <span className="label-text">Nimi</span>
                     </div>
                     <input type="fname" name="fname" placeholder="Seppo Taalasmaa" className="input input-bordered w-full" />
-                    </label>
+                    </label> */}
 
-                    <label className="mt-5 form-control w-full">
+                    <label className=" form-control w-full">
                     <div className="label">
                         <span className="label-text">Sähköposti</span>
                     </div>
