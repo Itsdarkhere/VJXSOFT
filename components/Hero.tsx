6db849d56c4c702c8380af6a0d744dd0e0a9ffc3
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-02-28 17:42:35 +0200

    Very nice

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import sss from '../public/sss.svg';
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 export default function Hero() {
 
     return (
-        <section className="lg:px-6 w-full flex justify-center my-12">
+        <section className="lg:px-6 w-full flex justify-center my-24">
             <div className="w-full lg:py-10 flex flex-col lg:flex-row justify-between max-w-7xl lg:bg-white lg:rounded-3xl px-6">
                 <div className="flex flex-col gap-2 justify-center items-start max-w-3xl">
                     <h3 className=" text-4xl font-bold tracking-tight text-gray-100 lg:text-gray-900 sm:text-6xl">Hoidetaanko yrityksesi digitaalinen läsnäolo kuntoon?</h3>
                     <h6 className=" mt-4 text-xl text-gray-300 lg:text-gray-500">Löydä asiakkaasi siellä, missä he ovat – hyödynnä dataohjautuvaa markkinointia ja innovatiivisia digistrategioita, jotka vievät yrityksesi seuraavalle tasolle.</h6>
-                    <button className=" mt-6 inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700">Ota yhteyttä</button>
+                    <button className=" mt-6 gap-2 flex flex-row items-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700">
+                        Ota yhteyttä
+                        <ArrowRightIcon className=" text-white w-4" />
+                    </button>
                 </div>
                 <Image className=" hidden lg:block" src={sss} alt="VJXSOFT" />
             </div>
