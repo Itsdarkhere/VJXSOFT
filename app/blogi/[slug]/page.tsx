a959a2fc92bdc520596cbaa01c00adbdc62d6b3a
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-03-30 11:11:47 +0100

    Blog base

---
+++ app/blogi/[slug]/page.tsx
@@ -0,0 +1,59 @@
+import { BlogItem } from "@/types";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { createClient } from "contentful";
+
+const client = createClient({
+    space: process.env.CONTENFUL_SPACE_ID!,
+    accessToken: process.env.CONTENT_DELIVERY_API!,
+})
+
+export async function generateStaticParams() {
+    const queryOptions = {
+        content_type: 'blog',
+        select: "fields.slug",
+    };
+    const articles = await client.getEntries(queryOptions);
+    return articles.items.map((article) => ({
+        slug: article.fields.slug,
+    }));
+}
+
+type BlogPageProps = {
+    params: {
+      slug: string;
+    };
+};
+
+const fetchBlogPost = async (slug: string): Promise<any> => {
+    const queryOptions = {
+        content_type: 'blog',
+        "fields.slug[match]": slug,
+    };
+    const queryResult = await client.getEntries(queryOptions);
+    return queryResult.items[0]
+}
+
+export default async function Blogi(props: BlogPageProps) {
+    const { params } = props;
+    const { slug } = params;
+    const article = await fetchBlogPost(slug);
+    const { title, date, content } = article.fields;
+    return (
+        <div className="min-h-screen p-24 flex flex-col justify-center">
+            <div className="max-w-2xl">
+                <h1 className="font-extrabold text-3xl mb-2">{title}</h1>
+                <p className="mb-6 text-slate-400 ">
+                    Posted on{" "}
+                    {new Date(date).toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                    })}
+                </p>
+            </div>
+            <div className="[&>p]:mb-8 [&>h2]:font-extrabold">
+                { documentToReactComponents(content) }
+            </div>
+        </div>
+    )
+}
--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -1,60 +1,20 @@
+import { BlogItem, BlogQueryResult } from "@/types";
+import { createClient } from "contentful";
+import Link from "next/link";
 
+const client = createClient({
+  space: process.env.CONTENFUL_SPACE_ID!,
+  accessToken: process.env.CONTENT_DELIVERY_API!,
+});
 
-export default function Blogs() {
+const getBlogEntries = async () => {
+  const entries = (await client.getEntries({ content_type: "blog" }));
+  return entries;
+};
 
-    const posts = [
-        {
-          id: 1,
-          title: 'Boost your conversion rate',
-          href: '#',
-          description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-          date: 'Mar 16, 2020',
-          datetime: '2020-03-16',
-          category: { title: 'Marketing', href: '#' },
-          author: {
-            name: 'Michael Foster',
-            role: 'Co-Founder / CTO',
-            href: '#',
-            imageUrl:
-              'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-          },
-        },
-        {
-          id: 2,
-          title: 'Boost your conversion rate',
-          href: '#',
-          description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-          date: 'Mar 16, 2020',
-          datetime: '2020-03-16',
-          category: { title: 'Marketing', href: '#' },
-          author: {
-            name: 'Michael Foster',
-            role: 'Co-Founder / CTO',
-            href: '#',
-            imageUrl:
-              'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-          },
-        },
-        {
-          id: 3,
-          title: 'Boost your conversion rate',
-          href: '#',
-          description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-          date: 'Mar 16, 2020',
-          datetime: '2020-03-16',
-          category: { title: 'Marketing', href: '#' },
-          author: {
-            name: 'Michael Foster',
-            role: 'Co-Founder / CTO',
-            href: '#',
-            imageUrl:
-              'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-          },
-        },
-    ];
+export default async function Blogs() {
+    const blogEntries = await getBlogEntries();
+    console.log("blogEntries: ", blogEntries);
 
     return (
         <section className=" w-full bg-white flex justify-center px-6">
@@ -67,45 +27,82 @@ export default function Blogs() {
                     </p>
                     </div>
                     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                    {posts.map((post) => (
-                        <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
-                        <div className="flex items-center gap-x-4 text-xs">
-                            <time dateTime={post.datetime} className="text-gray-500">
-                            {post.date}
-                            </time>
-                            <a
-                            href={post.category.href}
-                            className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                            >
-                            {post.category.title}
-                            </a>
-                        </div>
-                        <div className="group relative">
-                            <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                            <a href={post.href}>
-                                <span className="absolute inset-0" />
-                                {post.title}
-                            </a>
-                            </h3>
-                            <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
-                        </div>
-                        {/* <div className="relative mt-8 flex items-center gap-x-4">
-                            <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
-                            <div className="text-sm leading-6">
-                            <p className="font-semibold text-gray-900">
-                                <a href={post.author.href}>
-                                <span className="absolute inset-0" />
-                                {post.author.name}
-                                </a>
-                            </p>
-                            <p className="text-gray-600">{post.author.role}</p>
-                            </div>
-                        </div> */}
-                        </article>
-                    ))}
+                      {blogEntries.items.map((post: any) => {
+                        const { slug, title, date } = post.fields;
+                        return (
+                          <div key={slug} className=" text-zinc-500">
+                            <Link href={`/blogi/${slug}`}>
+                              <h2 className=" text-zinc-800 font-semibold">{title}</h2>
+                              <span>
+                                Posted on{" "}
+                                {new Date(date).toLocaleDateString("en-US", {
+                                  year: "numeric",
+                                  month: "long",
+                                  day: "numeric",
+                                })}
+                              </span>
+                            </Link>
+                          </div>
+                        );
+                      })}
                     </div>
                 </div>
             </div>
         </section>
     )
 }
+
+
+const posts = [
+  {
+    id: 1,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    id: 2,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    id: 3,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+];
