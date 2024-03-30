5bfd3c553533958a1c2db3aecd63bb6e9376dd17
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-03-30 12:30:18 +0100

    Better contact

--- app/otayhteytta/page.tsx
+++ app/otayhteytta/page.tsx
@@ -4,7 +4,7 @@ import Contact from "@/components/Contact";
 export default function Otayhteytta() {
 
     return (
-        <div className="flex min-h-screen bg-white flex-col justify-start items-center">
+        <div className="flex bg-white flex-col justify-start items-center">
             <Contact />
         </div>
     )
